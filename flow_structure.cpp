3c3fce269dc6c14670764b868bfc44dfafec0c35
Azack <1483932084@qq.com>
2026-08-02 01:41:14 +0000

    for and find 7

--- flow_structure.cpp
+++ flow_structure.cpp
@@ -2,18 +2,13 @@
 #include<ctime>
 #include<cmath>
 int main()
-{   int a =100;
-    do {
-        int b=a/100;
-        int c=(a%100)/10;
-        int d=a%10;
-        //std::cout<<b<<c<<d<<std::endl;
-        if(((std::pow(b, 3)+std::pow(c, 3)+std::pow(d, 3))==a)?a:0)
+{   for(int i=1;i<101;i++)
+    {
+        if(i/10==7||i%10==7||i%7==0)
         {
-            std::cout<<a<<std::endl;
+        std::cout << i<<std::endl;
         }
-        a++;
-    }while (a<1000);
+    }
     
     return 0;
 }
