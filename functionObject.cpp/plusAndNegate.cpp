6be3b334af5581e182581721e669531fa2cb179e
Azack <1483932084@qq.com>
2026-09-12 01:22:10 +0000

    greater

--- functionObject.cpp/plusAndNegate.cpp
+++ functionObject.cpp/plusAndNegate.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<functional>
+#include<vector>
+#include<algorithm>
 //negate取反仿函数
 //plus加法仿函数
 int main()
@@ -8,5 +10,20 @@ int main()
     std::cout<<n(50)<<std::endl;
     std::plus<int> p;
     std::cout<<p(10,20)<<std::endl;
+    std::greater<int> g;
+    std::cout<<g(201,21)<<std::endl;
+    std::vector<int> v;
+    for(int i=0;i<10;i++)
+    {
+        v.push_back(i);
+    }
+    //std::sort(v.begin(),v.end(),g);
+    std::sort(v.begin(),v.end(),std::greater<int>());
+    for(auto it = v.begin();it!=v.end();it++)
+    {
+        std::cout<<*it<<" ";
+    }
+    std::cout<<std::endl;
+
     return 0;
 }
